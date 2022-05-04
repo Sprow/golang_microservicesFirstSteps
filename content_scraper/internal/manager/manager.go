b85ed29d7e7a9af8864c263a7a1806d21dcdd236
Sprow <ts.sprow@gmail.com>
2022-05-04 16:04:46 +0300

    add content_parser microservice

--- content_scraper/internal/manager/manager.go
+++ content_scraper/internal/manager/manager.go
@@ -4,33 +4,62 @@ import (
 	"ContentScraper/internal/mongodb"
 	"ContentScraper/internal/scraper"
 	"context"
+	"github.com/streadway/amqp"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"log"
 )
 
-type SiteScraperManager struct {
-	scraper *scraper.SiteScraper
-	db      *mongodb.MongoDB
+type ContentScraperManager struct {
+	ch      *amqp.Channel
+	scraper *scraper.ContentScraper
+	DB      *mongodb.MongoDB
 }
 
-func NewSiteScraperManager(scraper *scraper.SiteScraper, db *mongodb.MongoDB) *SiteScraperManager {
-	return &SiteScraperManager{
+func NewContentScraperManager(ch *amqp.Channel, scraper *scraper.ContentScraper, db *mongodb.MongoDB) *ContentScraperManager {
+	return &ContentScraperManager{
+		ch:      ch,
 		scraper: scraper,
-		db:      db,
+		DB:      db,
 	}
 }
 
-func (m *SiteScraperManager) GetSiteContent(ctx context.Context, url string) error {
+func (m *ContentScraperManager) GetSiteContent(ctx context.Context, url string) error {
 	data, err := m.scraper.GetContent(url)
+	var status bool
 	if err != nil {
 		log.Println(err)
+		status = mongodb.StatusScrapFailed
+		_, err = m.DB.SaveContent(ctx, url, []byte{}, status)
 		return err
 	}
 	log.Printf("Get content from url %s", url)
-	err = m.db.SaveContent(ctx, data)
+	status = mongodb.StatusScrapDone
+	oid, err := m.DB.SaveContent(ctx, url, data, status)
 	if err != nil {
 		log.Println(err)
 		return err
 	}
 	log.Printf("Save content from url %s", url)
+	if status {
+		err = m.publishScrapedSiteID(oid)
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+	}
 	return nil
+}
+
+func (m *ContentScraperManager) publishScrapedSiteID(oid primitive.ObjectID) error {
+	err := m.ch.Publish(
+		"parser_direct", // exchange
+		"oid",           // routing key
+		false,           // mandatory
+		false,           // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(oid.Hex()),
+		})
+	log.Printf("send _oid `%s` to exchange `parser_direct`, routing key `oid`", oid)
+	return err
 }
--- content_scraper/internal/mongodb/mongodb.go
+++ content_scraper/internal/mongodb/mongodb.go
@@ -2,6 +2,7 @@ package mongodb
 
 import (
 	"context"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -16,17 +17,40 @@ func NewMongoDB(collection *mongo.Collection) *MongoDB {
 	}
 }
 
-type siteData struct {
-	ID   primitive.ObjectID `bson:"_id"`
-	Data []byte             `bson:"data"`
+const (
+	StatusScrapDone   = true
+	StatusScrapFailed = false
+)
+
+type SiteContent struct {
+	ID            primitive.ObjectID `bson:"_id" json:"id"`
+	URL           string             `bson:"url" json:"URL"`
+	Content       []byte             `bson:"content" json:"content"`
+	ScrapedStatus bool               `bson:"scrapedStatus" json:"scrapedStatus"`
 }
 
-func (db *MongoDB) SaveContent(ctx context.Context, data []byte) error {
-	d := siteData{
-		ID:   primitive.NewObjectID(),
-		Data: data,
+func (db *MongoDB) SaveContent(ctx context.Context, url string, content []byte, status bool) (primitive.ObjectID, error) {
+	newID := primitive.NewObjectID()
+	d := SiteContent{
+		ID:            newID,
+		URL:           url,
+		Content:       content,
+		ScrapedStatus: status,
 	}
-
 	_, err := db.collection.InsertOne(ctx, d)
-	return err
+	return newID, err
+}
+
+type HtmlBody struct {
+	Html []byte `json:"html"`
+}
+
+func (db *MongoDB) GetContentByOID(ctx context.Context, oid primitive.ObjectID) (HtmlBody, error) {
+	var content SiteContent
+	filter := bson.D{{"_id", oid}}
+	err := db.collection.FindOne(ctx, filter).Decode(&content)
+	if err != nil {
+		return HtmlBody{content.Content}, err
+	}
+	return HtmlBody{content.Content}, nil
 }
